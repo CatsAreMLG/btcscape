598341b2829789d54254ca5e43340d5bb14e0e95
CatsAreMLG <joshuadgon@gmail.com>
2020-08-22 18:28:25 -0500

    Add Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
-export default () => <div>Hello Next.js</div>;
+import Navbar from '../components/Navbar';
+
+const Index = () => (
+  <div>
+    <Navbar />
+    <h1>Welcome to BTCSCAPE</h1>
+  </div>
+);
+
+export default Index;
